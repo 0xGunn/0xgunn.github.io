0002b00785963e7f25e206adf826bd3a9a4e3a51
vieTins <nviettin48@gmail.com>
2026-02-09 10:50:26 +0700

    add research post

--- src/consts.ts
+++ src/consts.ts
@@ -28,10 +28,6 @@ export const NAV_LINKS: SocialLink[] = [
     href: '/authors',
     label: 'authors',
   },
-  {
-    href: '/about',
-    label: 'about',
-  },
 ]
 
 export const SOCIAL_LINKS: SocialLink[] = [
